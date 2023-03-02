d2cc439d7b6294bc453030401c5ace146e04bd87
Artur <68343991+at-chjnjd@users.noreply.github.com>
2023-03-02 20:36:15 +0100

    Update Weather.java

--- src/main/java/Weather.java
+++ src/main/java/Weather.java
@@ -3,55 +3,45 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Scanner;
 
 public class Weather {
 
     public static String getWeather(String message, Model model) throws IOException {
-        try {
-            URL url = new URL("http://api.openweathermap.org/data/2.5/weather?q=" + message + "&units=metric&appid=your_id_from_openWeather");
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-            connection.setConnectTimeout(5000);
-            connection.setReadTimeout(5000);
+        String apiKey = "YOUR_API_KEY_HERE"; // Replace with your API key
+        StringBuilder sb = new StringBuilder();
+        sb.append("http://api.openweathermap.org/data/2.5/weather?q=")
+                .append(message)
+                .append("&units=metric&appid=")
+                .append(apiKey);
+        URL url = new URL(sb.toString());
 
-            int responseCode = connection.getResponseCode();
-            if (responseCode != HttpURLConnection.HTTP_OK) {
-                throw new IOException("HTTP error code: " + responseCode);
+        try (Scanner in = new Scanner((InputStream) url.getContent())) {
+            String result = "";
+            while (in.hasNext()) {
+                result += in.nextLine();
             }
 
-            try (InputStream inputStream = connection.getInputStream()) {
-                Scanner in = new Scanner(inputStream);
-                String result = "";
-                while (in.hasNext()) {
-                    result += in.nextLine();
-                }
+            JSONObject object = new JSONObject(result);
+            model.setName(object.getString("name"));
 
-                JSONObject object = new JSONObject(result);
-                model.setName(object.getString("name"));
+            JSONObject main = object.getJSONObject("main");
+            model.setTemp(main.getDouble("temp"));
+            model.setHumidity(main.getDouble("humidity"));
 
-                JSONObject main = object.getJSONObject("main");
-                model.setTemp(main.getDouble("temp"));
-                model.setHumidity(main.getDouble("humidity"));
-
-                JSONArray getArray = object.getJSONArray("weather");
-                for (int i = 0; i < getArray.length(); i++) {
-                    JSONObject obj = getArray.getJSONObject(i);
-                    model.setIcon((String) obj.get("icon"));
-                    model.setMain((String) obj.get("main"));
-                }
-
-                return "City: " + model.getName() + "\n" +
-                        "Temperature: " + model.getTemp() + "C" + "\n" +
-                        "Humidity:" + model.getHumidity() + "%" + "\n" +
-                        "Main: " + model.getMain() + "\n" +
-                        "http://openweathermap.org/img/w/" + model.getIcon() + ".png";
+            JSONArray getArray = object.getJSONArray("weather");
+            for (int i = 0; i < getArray.length(); i++) {
+                JSONObject obj = getArray.getJSONObject(i);
+                model.setIcon((String) obj.get("icon"));
+                model.setMain((String) obj.get("main"));
             }
-        } catch (MalformedURLException e) {
-            throw new IOException("Invalid URL", e);
+
+            return "City: " + model.getName() + "\n" +
+                    "Temperature: " + model.getTemp() + "C" + "\n" +
+                    "Humidity:" + model.getHumidity() + "%" + "\n" +
+                    "Main: " + model.getMain() + "\n" +
+                    "http://openweathermap.org/img/w/" + model.getIcon() + ".png";
         }
     }
 }
