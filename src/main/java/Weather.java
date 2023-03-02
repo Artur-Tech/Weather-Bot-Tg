bb612673049310955425afb8b8e94e07031cf092
Artur <68343991+at-chjnjd@users.noreply.github.com>
2023-03-02 11:17:38 +0100

    Update Weather.java

--- src/main/java/Weather.java
+++ src/main/java/Weather.java
@@ -12,7 +12,7 @@ public class Weather {
 
     public static String getWeather(String message, Model model) throws IOException {
         try {
-            URL url = new URL("http://api.openweathermap.org/data/2.5/weather?q=" + message + "&units=metric&appid=6fff53a641b9b9a799cfd6b079f5cd4e");
+            URL url = new URL("http://api.openweathermap.org/data/2.5/weather?q=" + message + "&units=metric&appid=your_id_from_openWeather");
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
             connection.setConnectTimeout(5000);
